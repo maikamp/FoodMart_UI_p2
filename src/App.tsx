4b09ebee6fe8a59bc7173fb98854a9c14e495dd3
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-03-09 14:38:31 -0800

    Rest of the fixes of merge

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
 import EnhancedTable from './components/ItemList'
-import Register from './components/Register'
+// import Register from './components/Register'
+import AddRole from './components/Roles/addNew';
+import GetById from './components/Roles/getById';
+import LoginMain from './components/login';
+import GetAllRoles from './components/Roles/getAll';
 
 
 function App() {
@@ -12,8 +16,12 @@ function App() {
     <div className="App">
       <Navbar></Navbar>
      
-     <Register/>
+     {/*<Register/>*/}
       <EnhancedTable></EnhancedTable>
+        <AddRole/>
+        <GetById/>
+        <LoginMain/>
+        <GetAllRoles/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,41 +1,41 @@
-import * as React from 'react';
-import {useState, useEffect} from 'react';
-
-
-export default function Register(){
-    const [username, setUserrName] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const [role, setRole] = useState([]);
-
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-
-        const user = {username, password, email}
-
-        fetch("https://foodmartapi-1646848624483.azurewebsites.net/users/addUser", {
-            method:"POST",
-            headers:{"Content-type":"application/json"},
-            body:JSON.stringify(user)
-        }).then(()=>{
-            console.log("User Added")
-        })}
-    
-
-    
-
-
-    return (
-    <form onSubmit = {this.handleSubmit}> 
-     <input placeholder='username' id = 'loginText' ref ={username} />
-     <input placeholder='password' id = 'loginText' ref ={password} />
-     <input placeholder='email' id = 'loginText' ref = {email} />
-    <select>
-        <option value="1">1</option>
-        <option value="2">2</option>
-    </select>
-    <button id = 'registerButton' onClick={submitRegister}> Register </button>
-     <label> Register  </label>
-    </form>
-    )
-}
+// import * as React from 'react';
+// import {useState, useEffect} from 'react';
+//
+//
+// export default function Register(){
+//     const [username, setUserrName] = useState('');
+//     const [password, setPassword] = useState('');
+//     const [email, setEmail] = useState('');
+//     const [role, setRole] = useState([]);
+//
+//     const handleSubmit = (e)=>{
+//         e.preventDefault()
+//
+//         const user = {username, password, email}
+//
+//         fetch("https://foodmartapi-1646848624483.azurewebsites.net/users/addUser", {
+//             method:"POST",
+//             headers:{"Content-type":"application/json"},
+//             body:JSON.stringify(user)
+//         }).then(()=>{
+//             console.log("User Added")
+//         })}
+//
+//
+//
+//
+//
+//     return (
+//     <form onSubmit = {handleSubmit}>
+//      <input placeholder='username' id = 'loginText' ref ={username} />
+//      <input placeholder='password' id = 'loginText' ref ={password} />
+//      <input placeholder='email' id = 'loginText' ref = {email} />
+//     <select>
+//         <option value="1">1</option>
+//         <option value="2">2</option>
+//     </select>
+//     {/*<button id = 'registerButton' onClick={submitRegister}> Register </button>*/}
+//      <label> Register  </label>
+//     </form>
+//     )
+// }
