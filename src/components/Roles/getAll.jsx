efdf17dc01cadb54ac163798a3975bd869772684
Awaab Elamin <AwaabElamin@gmail.com>
2022-03-10 09:28:08 -0600

    updated get all

--- src/components/Roles/getAll.jsx
+++ src/components/Roles/getAll.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { Role } from '../../Models/Role';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 export default function GetAllRoles() {
     const [all, setAll] = useState();
     const getAll = async () => {
@@ -29,6 +32,42 @@ export default function GetAllRoles() {
 
 
         </table>
+        <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all',
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
 
     </div>
 }
